9d17662f7ab08f70bbc7199453077f1621ae7f7b
Maxwell Brown <mbrownet@gmail.com>
2023-03-20 11:17:39 -0400

    UX enhancements

--- assets/js/script.js
+++ assets/js/script.js
@@ -143,7 +143,7 @@ $("#task-form-modal").on("shown.bs.modal", () => {
 });
 
 // save button in modal was clicked
-$("#task-form-modal .btn-primary").click(() => {
+$("#task-form-modal .btn-save").click(() => {
   // get form values
   const taskText = $("#modalTaskDescription").val();
   const taskDate = $("#modalDueDate").val();
@@ -180,16 +180,18 @@ $(".card .list-group").sortable({
   helper: "clone",
 
   activate: function (event) {
-    console.log("activate", this);
+    $(this).addClass("dropover");
+    $(".bottom-trash").addClass("bottom-trash-drag");
   },
   deactivate: function (event) {
-    console.log("deactivate", this);
+    $(this).removeClass("dropover");
+    $(".bottom-trash").removeClass("bottom-trash-drag");
   },
   over: function (event) {
-    console.log("over", event.target);
+    $(event.target).addClass("dropover-active");
   },
   out: function (event) {
-    console.log("out", event.target);
+    $(event.target).removeClass("dropover-active");
   },
   update: function (event) {
     // array to store the task data in
@@ -258,6 +260,12 @@ const auditTask = function (taskEl) {
     $(taskEl).addClass("list-group-item-warning");
   }
 };
+
+setInterval(function () {
+  $(".card .list-group-item").each(function (index, el) {
+    auditTask(el);
+  });
+}, 1000 * 60 * 30);
 
 // load tasks for the first time
 loadTasks();
